5cc93deea6b705604e8a34496907cf0924822baa
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-04-07 17:01:22 +0100

    Mentioned libnotify-bin dependency

--- notify-apache-errors.sh
+++ notify-apache-errors.sh
@@ -1,4 +1,5 @@
 # use ubuntu's notification system to let us know when there's something new in the apache error log
+# requires package libnotify-bin
 tail -n0 -f /var/log/apache2/error.log | while read line
 do
 	body=`echo $line | cut -f 9- -d ' '`
