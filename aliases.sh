9c22a1b5c710c20f63e7c7e3cf7a42f875412d4d
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-06-10 10:53:42 +0100

    Added Peter's "cdup" function and aliased to '..'

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,3 @@
-alias ..='cd ..' # go up a dir
-
 alias n='nautilus `pwd` &' # open the working dir in nautilus
 
 alias g='git' # as i use git so much
@@ -47,3 +45,19 @@ goo () { w3m http://gog.is/"$*"; }
 
 # google text to speech
 say(){ mplayer -user-agent Mozilla "http://translate.google.com/translate_tts?tl=en&q=$(echo $* | sed 's#\ #\+#g')" > /dev/null 2>&1 ; }
+
+
+
+# go up a number of directories
+# usage: cdup == "cd .."; cdup 5 == "cd .." x 5
+# from picklepete
+cdup() {
+  levels=${1-1}
+  while ((levels--)); do
+        cd ..
+  done
+}
+
+# old "go up a dir" alias
+# alias ..='cd ..' # go up a dir
+alias ..='cdup' # go up x dirs
