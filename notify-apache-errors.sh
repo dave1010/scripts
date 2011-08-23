90f61512ea09dd0027c2b35ab79e4438f72da9d9
Dave Hulbert <dave.hulbert@basecreativeagency.com>
2011-08-23 11:59:04 +0100

    Don't show stack traces in notify-apache-errors

--- notify-apache-errors.sh
+++ notify-apache-errors.sh
@@ -1,6 +1,6 @@
 # use ubuntu's notification system to let us know when there's something new in the apache error log
 # requires package libnotify-bin
-tail -n0 -f /var/log/apache2/error.log | while read line
+tail -n0 -f /var/log/apache2/error.log | egrep 'Notice|Warning|Error|Fatal' | while read line
 do
 	body=`echo $line | cut -f 9- -d ' '`
 	notify-send -c im.received -i error "Apache / PHP error" "$body"
