fb37f67c97b1cbc713bf0e5c2e92e7d3fb9b210a
base <base@base100.(none)>
2013-01-11 14:16:32 +0000

    Update screen fixy thing

--- fix-my-screen-resolution.sh
+++ fix-my-screen-resolution.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-xrandr --addmode CRT1 1680x1050
-xrandr --output CRT1 --mode 1680x1050
-xrandr --output CRT1 --left-of CRT2
+xrandr --addmode DVI-0 1680x1050
+xrandr --output DVI-0 --mode 1680x1050
+xrandr --output DVI-0 --left-of DVI-1
