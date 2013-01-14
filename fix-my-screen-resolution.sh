2906983b91d5b263a5245bb7976b7d72320bbbbb
base <base@base100.(none)>
2013-01-14 09:22:44 +0000

    Comment on how to fix screen res on login

--- fix-my-screen-resolution.sh
+++ fix-my-screen-resolution.sh
@@ -2,3 +2,11 @@
 xrandr --addmode DVI-0 1680x1050
 xrandr --output DVI-0 --mode 1680x1050
 xrandr --output DVI-0 --left-of DVI-1
+
+# http://askubuntu.com/questions/63681/how-can-i-make-xrandr-customization-permanent
+
+
+# Edit: /etc/lightdm/lightdm.conf
+# sudo ln -s ~/scripts/fix-my-screen-resolution.sh /usr/share/
+# display-setup-script=/usr/share/mycustomloginvideo.sh
+# session-setup-script=/usr/share/mycustomdesktopvideo.sh
